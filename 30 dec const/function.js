3597388962b526b1e68b48d796cbe3695caafe11
Abhinand62 <abhinandaby504@gmail.com>
2023-01-02 14:58:59 +0530

    some changes

---
+++ 30 dec const/function.js
@@ -0,0 +1,11 @@
+const student = {
+    name : 'student a',
+    mark1: 40,
+    mark2: 50
+};
+
+const values = Object.values(student);
+values.forEach(value=>{
+    console.log(value);
+
+});
--- 30 dec const/matrix.js
+++
@@ -1,5 +0,0 @@
-const m1 = [
-    [1, 2],
-    [3, 4]
-];
-console.log(m1);
